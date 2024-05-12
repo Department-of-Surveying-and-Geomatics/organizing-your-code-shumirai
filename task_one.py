111fafda9f0780916631f3e110a979850e2c536d
r2213431p <169069860+r2213431p@users.noreply.github.com>
2024-05-12 10:49:14 +0200

    assignment task_one.py
    l'm done

--- task_one.py
+++ task_one.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def get_file():
+def plot_data(file_path):
+    """
+    Function to plot a scatterplot of data from a file.
 
-    file_name = ("peters.csv")
+    Args:
+        file_path (str): Path to the data file.
+    """
+    # Reading the data from the file using pandas
+    data = pd.read_csv(file_path, skiprows=1, names=['x', 'y'], delimiter=', ', engine='python')
 
-def plot_data(file_obj):
+    # Creating a scatter plot of the data using matplotlib
+    plt.scatter(data['x'], data['y'])
+    plt.xlabel('x')  # Setting the label for the x-axis
+    plt.ylabel('y')  # Setting the label for the y-axis
+    plt.title('Scatter Plot')  # Setting the title of the plot
+    plt.show()  # Displaying the plot
 
-    data = pd.read_csv("peters.csv")
-    x = data.iloc[:,0]
-    y = data.iloc[:,1]
+# File path
+file_path = 'x_y_coordinates.txt'  # File path of the data file
 
-plt.figure(figsize=(8,6))
-plt.scatter(x_values,y_values,c="blue")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("scatterplot of data")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
+# Call the plot_data function and pass the file path as the input parameter
+plot_data(file_path)
